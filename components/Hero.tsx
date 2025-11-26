614392ad4170adb5b3c4bda264ae861d415d21be
pratik hirapara <pratikhirapara13@gmail.com>
2025-11-26 12:27:19 -0500

    feat: add dark black theme for the layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ export const Hero = () => {
             <Spotlight  className="top-28 left-80
             h-[80vh] w-[50vw]" fill="blue"/>
         </div>
-        <div className="relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black">
+        <div className=" h-screen w-full  dark:bg-black-100 bg-white dark:bg-grid=white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
       <div
         className={cn(
           "absolute inset-0",
@@ -23,10 +23,7 @@ export const Hero = () => {
         )}
       />
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-        Backgrounds
-      </p>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100 "/>
     </div>
     </div>
       
